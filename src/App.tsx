c48ad9778a28af97b298a0558f8472736bbd71e5
Venkatesh Venkatesh <venkatesh.venkatesh@optus.com.au>
2021-06-20 12:59:34 +1000

    webpack plugins for dev, prod and stage env sertup

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import ReactLogo from './react.png'
 import Logo from './logo.svg'
 import { ClickCounter } from './ClickCounter'
 export const App = () => {
-  const ve ="vvvv"
   return (
     <>
       <h1>
-        VT Tech React First Type Script - {process.env.NODE_ENV}{' '}
+        VT Tech React First Type Script - {process.env.NODE_ENV}
         {process.env.name}
       </h1>
       <img alt="React logo" src={ReactLogo} width="10%" height="10%" />
